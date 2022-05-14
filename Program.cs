8c5c8d94ed171f3b937e8e12b540684a6de12b95
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-05-14 01:15:05 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string senha;
+
+Console.WriteLine("Olá, usuário. Por favor, digite sua senha:");
+senha = Console.ReadLine()!;
+
+if (senha == "1234abcd")
+{
+    Console.WriteLine("Acesso permitido");
+}
+
+else 
+{
+    Console.WriteLine("Acesso negado");
+}
